e46f3ee936b63a8c72626c90bac8d4872363c762
吴子豪 <vortez.wohl@gmail.com>
2025-04-12 13:19:52 +0800

    添加 md5 长文本哈希

--- src/diff.py
+++ src/diff.py
@@ -1,5 +1,6 @@
 import numpy as np
 from src.lda_model import predict_topics
+from src.text_hash import long_text_hash
 
 topic_dict_cache = dict()
 
@@ -15,9 +16,10 @@ def kl_divergence(p: np.ndarray, q: np.ndarray) -> np.float32:
 
 def kl_divergence_between_texts(text_a: str, text_b: str) -> np.float32:
     global topic_dict_cache
-    topic_dist_a, topic_dist_b = topic_dict_cache.get(text_a, predict_topics(text_a)), topic_dict_cache.get(text_b, predict_topics(text_b))
-    if text_a not in topic_dict_cache.keys():
-        topic_dict_cache[text_a] = topic_dist_a
-    if text_b not in topic_dict_cache.keys():
-        topic_dict_cache[text_b] = topic_dist_b
+    hash_a, hash_b = long_text_hash(text_a), long_text_hash(text_b)
+    topic_dist_a, topic_dist_b = topic_dict_cache.get(hash_a, predict_topics(text_a)), topic_dict_cache.get(hash_b, predict_topics(text_b))
+    if hash_a not in topic_dict_cache.keys():
+        topic_dict_cache[hash_a] = topic_dist_a
+    if hash_b not in topic_dict_cache.keys():
+        topic_dict_cache[hash_b] = topic_dist_b
     return kl_divergence(topic_dist_a, topic_dist_b)
