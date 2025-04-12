096f08961a4903e3ce6edc08331fae24440414a5
吴子豪 <vortez.wohl@gmail.com>
2025-04-12 11:10:15 +0800

    添加数据预处理逻辑

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,2 @@
 from .diff import diff_between_texts
+from .read_file import read_file, get_files
--- src/read_file.py
+++ src/read_file.py
@@ -1,3 +1,11 @@
+import os
+
+
 def read_file(path: str, encoding: str = 'utf-8') -> str:
     with open(path, mode='r', encoding=encoding) as f:
         return f.read()
+
+
+def get_files(path: str) -> list:
+    entries = os.listdir(path)
+    return [entry for entry in entries if os.path.isfile(os.path.join(path, entry))]
