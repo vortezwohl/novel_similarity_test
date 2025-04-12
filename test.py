aa9acf259a8e569425ee856089a8d0da79324125
吴子豪 <vortez.wohl@gmail.com>
2025-04-12 18:57:06 +0800

    update 测试脚本

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import os.path
+import time
 from pprint import pp
+
+import pandas as pd
 
 from src import get_files, read_file, kl_divergence_between_texts
 
@@ -21,17 +24,29 @@ print(len(negatives))
 print(len(_all))
 
 samples = _all
+diff_results = list[dict]()
+eval_results = list[dict]()
 
-diffs = []
-for sample in samples:
-    for doc in _all:
-        pass_flag_doc = True
-        if doc in negatives:
-            pass_flag_doc = False
+SLICE = 2
+
+for sample in samples[:SLICE]:
+    diffs = []
+    for doc in _all[:SLICE]:
+        pass_flag_doc = doc not in negatives
         _diff = kl_divergence_between_texts(sample[1], doc[1])
         print(f'diff("{sample[0]}", "{doc[0]}")={_diff}')
-        diffs.append((doc[0], pass_flag_doc, _diff))
+        if sample != doc:
+            diffs.append((doc[0], pass_flag_doc, _diff))
+        diff_results.append({
+            'novel_p': sample[0],
+            'novel_p_passed': sample not in negatives,
+            'novel_q': doc[0],
+            'novel_q_passed': pass_flag_doc,
+            'kl_divergence(p, q)': _diff
+        })
     diffs.sort(key=lambda x: x[2])
     _split = min(len(positives), len(negatives))
     pp(diffs[:_split])
     print(f'pass_rate("{sample[0]}")={len([x for x in diffs[:_split] if x[1]])/float(_split)}')
+
+pd.DataFrame(diff_results).to_csv(f'output/diff_results_{time.time()}.csv')
