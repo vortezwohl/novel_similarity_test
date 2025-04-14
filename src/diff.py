81211a74c1eb1f9a3e7392e53078d10b16644b3e
吴子豪 <vortez.wohl@gmail.com>
2025-04-14 17:33:48 +0800

    update 使用 teximilar

--- src/diff.py
+++ src/diff.py
@@ -1,20 +1,13 @@
 import numpy as np
+from teximilar.similarity.distribution_based import js_divergence
+
 from src.lda_model import predict_topics
 from src.text_hash import long_text_hash
 
 topic_dict_cache = dict()
 
 
-def kl_divergence(p: np.ndarray, q: np.ndarray) -> np.float32:
-    if not isinstance(p, np.ndarray):
-        p = np.asarray(p)
-    if not isinstance(q, np.ndarray):
-        q = np.asarray(q)
-    q = np.where(q == 0, 1e-10, q)
-    return np.sum(p * np.log(p / q))
-
-
-def kl_divergence_between_texts(text_a: str, text_b: str) -> np.float32:
+def distance_between_texts(text_a: str, text_b: str, operator: callable = js_divergence) -> np.float32:
     global topic_dict_cache
     hash_a, hash_b = long_text_hash(text_a), long_text_hash(text_b)
     topic_dist_a, topic_dist_b = topic_dict_cache.get(hash_a, predict_topics(text_a)), topic_dict_cache.get(hash_b, predict_topics(text_b))
@@ -22,4 +15,4 @@ def kl_divergence_between_texts(text_a: str, text_b: str) -> np.float32:
         topic_dict_cache[hash_a] = topic_dist_a
     if hash_b not in topic_dict_cache.keys():
         topic_dict_cache[hash_b] = topic_dist_b
-    return kl_divergence(topic_dist_a, topic_dist_b)
+    return operator(topic_dist_a, topic_dist_b)
--- src/read_file.py
+++ src/read_file.py
@@ -15,4 +15,4 @@ def read_file(path: str, encoding: str = 'utf-8') -> str:
 
 def get_files(path: str) -> list:
     entries = os.listdir(path)
-    return [entry for entry in entries if os.path.isfile(os.path.join(path, entry))]
+    return [os.path.join(path, entry) for entry in entries if os.path.isfile(os.path.join(path, entry))]
